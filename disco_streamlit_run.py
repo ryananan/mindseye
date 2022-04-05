9367af0c4072b7d54900a3178fc56270d72488e7
RYAN-MX <xumuhan@hotmail.com>
2022-04-05 20:52:55 +0800

    Revert "Change things to pytorch3d lite"
    This reverts commit 9f384bf1522ec02289fd7026c78aa4987a60a207.

--- disco_streamlit_run.py
+++ disco_streamlit_run.py
@@ -79,7 +79,6 @@ sys.path.append(".")
 sys.path.append("./taming-transformers")
 sys.path.append("./disco-diffusion")
 sys.path.append("./AdaBins")
-sys.path.append('./pytorch3d-lite')
 # sys.path.append('./pytorch3d')
 
 import os
@@ -154,7 +153,7 @@ from midas.midas_net import MidasNet
 from midas.midas_net_custom import MidasNet_small
 from midas.transforms import Resize, NormalizeImage, PrepareForNet
 import torch
-import py3d_tools as p3dT
+import pytorch3d.transforms as p3dT
 import disco_xform_utils as dxf
 import argparse
 
